e6a0e99057cbd95290fcf1ed7a0943300ec3df80
eliotttesnier <eliott.tesnier@epitech.eu>
2024-04-17 18:14:05 +0200

    [ADD] jump

--- Assets/Scripts/move.cs
+++ Assets/Scripts/move.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class move : MonoBehaviour
 {
-    // Update is called once per frame
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
     void Update()
     {
+    if (isGrounded) {
+        if (Input.GetButtonDown("Jump"))
+            rb.AddForce(Vector3.up * jumpHeight)
+    }
         if (Input.GetKey(KeyCode.A))
             transform.Translate(-0.1f, 0f, 0f);
         if (Input.GetKey(KeyCode.D))
@@ -15,5 +25,17 @@ public class move : MonoBehaviour
             transform.Translate(0f, 0f, 0.1f);
         if (Input.GetKey(KeyCode.S))
             transform.Translate(0f, 0f, -0.1f);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Ground")
+            isGrounded = true;
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.tag == "Ground")
+            isGrounded = false;
     }
 }
