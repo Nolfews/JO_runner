03226af65076d630c57648c4552484a215b4b1aa
Eliott <eliott.tesnier@epitech.eu>
2024-04-18 10:21:20 +0200

    move fix

--- Assets/Scripts/move.cs
+++ Assets/Scripts/move.cs
@@ -7,7 +7,7 @@ public class move : MonoBehaviour
     void Update()
     {
         transform.Translate(-0.1f, 0f, 0f);
-        if (Input.GetKey(KeyCode.A) && )
+        if (Input.GetKey(KeyCode.A))
             transform.Translate(0f, 0f, -0.1f);
         if (Input.GetKey(KeyCode.D))
             transform.Translate(0f, 0f, 0.1f);
