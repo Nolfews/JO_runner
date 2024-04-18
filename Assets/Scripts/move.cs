ffdbc6b34825a8dc92e30606485dee3aad382c0b
carlossobuaro <carlos.so-buaro@epitech.eu>
2024-04-18 10:54:53 +0200

    .

--- Assets/Scripts/move.cs
+++ Assets/Scripts/move.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class move : MonoBehaviour
 {
     void Update()
     {
         transform.Translate(-10f * Time.deltaTime, 0f, 0f);
-        if (Input.GetKey(KeyCode.A) && transform.position.z > -4)
+        if ((Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) && transform.position.z > -4)
             transform.Translate(0f, 0f, -25f * Time.deltaTime);
-        if (Input.GetKey(KeyCode.D) && transform.position.z < 4)
+        if ((Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) && transform.position.z < 4)
             transform.Translate(0f, 0f, 25f * Time.deltaTime);
     }
 }
