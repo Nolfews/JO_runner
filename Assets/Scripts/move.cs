193b8b940d981f99c72ea69f97eabafa603abbc0
eliotttesnier <eliott.tesnier@epitech.eu>
2024-04-18 09:27:41 +0200

    Move script

--- Assets/Scripts/move.cs
+++ Assets/Scripts/move.cs
@@ -4,40 +4,12 @@ using UnityEngine;
 
 public class move : MonoBehaviour
 {
-    public float jumpHeight = 7f;
-    public bool isGrounded;
-    private Rigidbody rb;
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
-
     void Update()
     {
-    if (isGrounded) {
-        if (Input.GetButtonDown("Jump"))
-            rb.AddForce(Vector3.up * jumpHeight);
-    }
-    if (Input.GetKey(KeyCode.A))
-        transform.Translate(-0.1f, 0f, 0f);
-    if (Input.GetKey(KeyCode.D))
-        transform.Translate(0.1f, 0f, 0f);
-    if (Input.GetKey(KeyCode.W))
-        transform.Translate(0f, 0f, 0.1f);
-    if (Input.GetKey(KeyCode.S))
-        transform.Translate(0f, 0f, -0.1f);
-    }
-
-    void OnCollisionEnter(Collision other)
-    {
-        if (other.gameObject.tag == "Ground")
-            isGrounded = true;
-    }
-
-    void OnCollisionExit(Collision other)
-    {
-        if (other.gameObject.tag == "Ground")
-            isGrounded = false;
+        transform.Translate(0f, 0f, 0.1f) * deltaTime;
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(-0.1f, 0f, 0f) * deltaTime;
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(0.1f, 0f, 0f) * deltaTime;
     }
 }
