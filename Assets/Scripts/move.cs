d78b945e333664a2625ad3ab2135823ceff5aefa
eliotttesnier <eliott.tesnier@epitech.eu>
2024-04-18 09:38:55 +0200

    move script?

--- Assets/Scripts/move.cs
+++ Assets/Scripts/move.cs
@@ -6,10 +6,10 @@ public class move : MonoBehaviour
 {
     void Update()
     {
-        transform.Translate(0f, 0f, 0.1f);
+        transform.Translate(0.1f, 0f, 0f);
         if (Input.GetKey(KeyCode.A))
-            transform.Translate(-0.1f, 0f, 0f);
+            transform.Translate(0f, 0f, 0.1f);
         if (Input.GetKey(KeyCode.D))
-            transform.Translate(0.1f, 0f, 0f);
+            transform.Translate(0f, 0f, -0.1f);
     }
 }
