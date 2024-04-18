b488639d996ecc47c8da70f9dc581415eb93aa74
Eliott <eliott.tesnier@epitech.eu>
2024-04-18 10:43:02 +0200

    move better

--- Assets/Scripts/move.cs
+++ Assets/Scripts/move.cs
@@ -6,10 +6,10 @@ public class move : MonoBehaviour
 {
     void Update()
     {
-        transform.Translate(-0.1f, 0f, 0f);
+        transform.Translate(-10f * Time.deltaTime, 0f, 0f);
         if (Input.GetKey(KeyCode.A) && transform.position.z > -4)
-            transform.Translate(0f, 0f, -0.1f);
+            transform.Translate(0f, 0f, -25f * Time.deltaTime);
         if (Input.GetKey(KeyCode.D) && transform.position.z < 4)
-            transform.Translate(0f, 0f, 0.1f);
+            transform.Translate(0f, 0f, 25f * Time.deltaTime);
     }
 }
